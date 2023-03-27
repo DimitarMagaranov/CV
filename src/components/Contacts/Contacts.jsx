cde47372ca7763e6c1e11ee49aa9e1d06e79a9f9
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2023-03-27 23:37:59 +0300

    added header and contacts

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+
+import './Contacts.css';
+
+const Contacts = () => {
+    return (
+        <div id="contacts-ctr">
+            <p>
+                <i class="fas fa-map-marker-alt"></i> Sofia, Bulgaria
+            </p>
+            <p>
+                <Link to={'https://www.linkedin.com/in/dimitar-magaranov-101019207/'}>
+                    <i class="fab fa-linkedin"></i> linkedin.com/in/dimitar-magaranov-101019207
+                </Link>
+            </p>
+            <p>
+                <Link to={'mailto:dimitar.magaranov1@gmail.com'}>
+                    <i class="fas fa-envelope"></i> dimitar.magaranov1@gmail.com
+                </Link>
+            </p>
+            <p>
+                <Link to={'https://github.com/DimitarMagaranov'}>
+                    <i class="fab fa-github"></i> github.com/DimitarMagaranov
+                </Link>
+            </p>
+            <p>
+                <i class="fas fa-phone"></i> +359877005195
+            </p>
+        </div>
+    );
+};
+
+export default Contacts;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div id="header-ctr">
+            <div id="img-ctr">
+                <img src="profile-pic.jpg" alt="" />
+            </div>
+            <div id="name-summary-ctr">
+                <h1>Dimitar Magaranov</h1>
+                <p>
+                    I am a self-driven and motivated student, possessing great analytical skills, ability to work in team environments, and strong
+                    attention in detail. I am looking forward to work with a company that deals in development new technologies and advance my skills
+                    as a developer while also to have a hand in developing the innovations of tomorrow
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
